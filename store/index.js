6e3744eb4657d3c7bbfc71f6c155df0eef7a009b
F145h <its.f145h@gmail.com>
2019-10-07 12:01:50 +0300

    + basic store

---
+++ store/index.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import Vue from 'vue';
+import Vuex from 'vuex';
+Vue.use(Vuex);
+export const state = () => {
+    return {
+        authUser: null,
+    };
+};
+export const mutations = {
+    SET_USER: function (state, user) {
+        state.authUser = user;
+    },
+};
+export const actions = {
+    // nuxtServerInit is called by Nuxt.js before server-rendering every page
+    async nuxtServerInit({ dispatch, commit }, { req }) {
+        if (req.session) {
+            if (req.session.authUser)
+                commit('SET_USER', req.session.authUser);
+        }
+        //req.session.touch();
+    },
+    async logout({ commit }) {
+        await axios.post('/api/logout');
+        commit('SET_USER', null);
+    }
+};
